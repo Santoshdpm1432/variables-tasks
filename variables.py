064b4e3c3094621ea5555364d6bd1c12db939591
santosh kumar agepogu <santoshkumaragepogu484@gmail.com>
2025-03-17 00:50:22 -0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,126 @@
+#variables
+#username = "asant647"
+#_username = "asant647"
+#7username = "asant647"
+#user_name123 = "asant647"
+#print(username)
+
+#commenting
+# num_1 = 10 #assigned value to a variable 1
+# num_2 = 20 #assigned value to a variable 2
+# print(num_1 + num_2) 
+''' i actually take tow variables to perform and addition
+first variable i take num_1
+second variable i take num_2
+then i perform addition operation'''
+
+#data_types
+#integer
+# num_1 = 10
+# print(type(num_1))
+# num_2 = 20
+# print(type(num_2))
+# result = (num_1 + num_2)
+# print(result)
+# print(type(result))
+
+
+# float
+
+# height = 5.66
+# width = 52.66
+# print(type(height))
+# print(height*width)
+
+# height = 5.66
+# width = 52.66
+# print(type(height))
+# print(height + width)
+
+
+#strings
+
+# word_1 = 'santosh kumar will attend the class dialy'
+# word_2 = "santosh kumar should attend the class at 8'o clock"
+# word_3 = """santosh kumar should need to attend at 8'o clock but ,sometimes it is not possible to attend in that time"""
+# print(type(word_1))
+# print(type(word_2))
+# print(type(word_3))
+# print(word_1)
+# print(word_2)
+# print(word_3)
+
+#lists
+
+# data_1 = [1,2,3,4,5, "santosh", 5.5, ]
+# data_2 = data_1.append("santosh_kumar_practicing_pyhton")
+# print(type(data_1))
+# print(id(data_1))
+# print(id(data_2))
+
+# data_1 = [1,2,3,4,5,5.6,(7,8,"santosh"),[1,2,3,4]]
+# data_1.append("santosh_kuamr")
+# data_1.append(4.5)
+# #data_1.append(6,7,8)#here if we add numbers instead of string we will get error
+# print(type(data_1))
+# #print(type(data_2))
+# print(id(data_1))
+# print(id(data_1))
+# print(id(data_1))
+# print(id(data_1))
+
+
+#tuple
+
+# tuple_1 = ([1,2,3,4],(1,2,3,4),4.5,"santosh")
+# print(tuple_1)
+# print(type(tuple_1))
+
+#sets
+
+
+
+# set_3 = {(1,2,3),1,2,3,"santosh","somesh","ujesh",5.6,5,6,7,8}
+# print(set_3)
+# print(type(set_3))
+
+#dictionary
+# dictionary_1 = {
+#     "username1":"password1",
+#      "username2":"password2",
+#      "username3":"password3",
+#     (1,2,3) : 1234,
+#     1:1234}
+# print(dictionary_1)
+# print(type(dictionary_1))
+
+
+#complex_data_type
+
+# imagine_1 = 10 + 5j
+# print(type(imagine_1))
+# print(imagine_1)
+
+# Boolean
+
+# one_value = True
+# print(type(one_value))
+
+# print(bool(0))
+# print(bool(1))
+
+
+
+
+#day_1 task:
+
+'''Create a Python script with both single-line and multi-line comments
+explaining the purpose of the script'''
+television = 20000 #television cost is 20000
+iphone = 120000 # iphone cost is 120000
+refridrator = 20000 # refridrator cost is 20000
+total_cost = television + iphone + refridrator 
+print(total_cost)
+'''i purchased television,
+iphone&refridrator today the total cost of today's shopping is 
+i will mention below'''
